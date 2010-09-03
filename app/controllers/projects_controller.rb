03b3f7574b1b39f9227c950c3f24dcb8217a4d4a
AndyDiep <namdiep@gmail.com>
2010-09-03 13:50:30 -0400

    Users should be redirected back to the project page if no resource found

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,5 +1,7 @@
 class ProjectsController < ApplicationController
 	
+	before_filter :find_project, :only => [:show, :edit, :update, :destroy]	
+	
 	def index
 		@projects = Project.all
 	end
@@ -20,19 +22,13 @@ class ProjectsController < ApplicationController
 	end
 
 	def show
-	  @project = Project.find(params[:id])
+	  
 	end
 
 	def edit
-	  @project = Project.find(params[:id])
+	  
 	end
 
-	def update
-	       @project = Project.find(params[:id])
-	       @project.update_attributes(params[:project])
-	       flash[:notice] = "Project has been updated."
-	       redirect_to @project
-	end
        
 	def update
 	      @project = Project.find(params[:id])
@@ -46,11 +42,19 @@ class ProjectsController < ApplicationController
 	end       
      
 	def destroy
-		@project = Project.find(params[:id])
+	
 		@project.destroy
 		flash[:notice] = "Project has been deleted."
 		redirect_to projects_path
 	end    
+	
+	private
+	def find_project
+		@project = Project.find(params[:id])
+		rescue ActiveRecord::RecordNotFound
+		flash[:error] = "The project you were looking for could not be found."
+		redirect_to projects_path
+	end	
 	
 
 	
